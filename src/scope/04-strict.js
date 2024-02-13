50c9772dedb9142c131d6352331cc9727caf7f31
Santiago <santiarango90@gmail.com>
2024-02-12 20:52:06 -0500

    archivos 4 y 5

---
+++ src/scope/04-strict.js
@@ -0,0 +1,11 @@
+//modo estricto
+'use strict'
+pi = 3.1416;
+console.log(pi);
+
+//modo estricto dentro de una funcion
+function miFuncion(){
+  'use strict';
+  return pi =3.1416
+}
+console.log(miFuncion());
